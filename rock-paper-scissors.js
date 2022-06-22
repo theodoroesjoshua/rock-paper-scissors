607e3a5a1253da184aaa2756a9cc715c43f9a897
Joshua <theodoroesjoshua@gmail.com>
2022-06-22 20:04:32 +0200

    Implement computerPlay

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+// Return a random number from 1 to the given parameter number
+// including the numbers on edge
+function random(num) {
+  return Math.floor(Math.random() * num);
+}
+
+function computerPlay() {
+  const randomNumber = random(3);
+  if (randomNumber === 1) {
+    console.log("Paper");
+  }
+  else if (randomNumber === 2) {
+    console.log("Rock");
+  }
+  else {
+    console.log("Scissors");
+  }
+}
+computerPlay();
